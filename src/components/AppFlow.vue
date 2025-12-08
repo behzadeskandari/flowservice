cfe0af9adc38dc7592d08c7ec26939d7d0637508
105624033 <behzad.b.i.g@gmail.com>
2025-12-08 07:52:21 +0330

    cmt

--- src/components/AppFlow.vue
+++ src/components/AppFlow.vue
@@ -22,7 +22,8 @@
       </button>
       <span
         class="px-3 py-2 bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 text-white font-semibold rounded-xl shadow-lg hover:from-orange-500 hover:via-orange-600 hover:to-orange-700 transition duration-300 ease-in-out">
-        <font-awesome-icon :icon="faSave" style="color: white;" />
+        <font-awesome-icon v-if="store.autoSaveEnabled()" :icon="faSave" style="color: white;" />
+        <font-awesome-icon v-else :icon="faSave" style="color: red;" />
 
         ذخیره خودکار: {{ store.autoSaveEnabled() ? 'فعال' : 'غیرفعال' }}
       </span>
